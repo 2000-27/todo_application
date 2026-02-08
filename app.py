88b478f1ff0b49480caf519159e74ba8ab5a9ca9
NEETU JANGID <123540454+2000-27@users.noreply.github.com>
2026-02-08 19:34:01 +0530

    Update app.py file

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from config import db_config
 from models import UserModel ,TaskModel
 
 app = Flask(__name__)
+# this is comment i am adding in my coe 
 db_config(app)
 
 app.register_blueprint(task_bp)
