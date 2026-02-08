287323194a412231cfe7a95c7bf1d1ed6030ffef
neetu <jangid2000neetu@gmail.com>
2026-02-08 18:45:02 +0530

    changes in task

--- view/task_view.py
+++ view/task_view.py
@@ -9,7 +9,8 @@ task_bp = Blueprint("task bp",__name__,url_prefix="/api")
 @task_bp.route("/create_task",methods=["GET"])
 @token_required
 def create_task(current_user):
-    
+    print(current_user)
+    print("we are createing a task")
     
     return jsonify(
         {
